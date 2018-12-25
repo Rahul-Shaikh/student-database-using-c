bf6f76640abef1bcf8b1d2a28b7b6f85ec011253
rahul5720 <rahulshaikh5720.ac@gmail.com>
2018-12-26 00:53:27 +0530

    Added the feature to search

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int sl = 0;
 
@@ -13,10 +14,11 @@ typedef struct student student;
 
 student def;
 
+void search_by_name(FILE *fr, char *a);
 void input(FILE *fa);
 void output(FILE *fr);
 void entry_no(FILE *fr);
-
+void search_by_roll(FILE *fr, char *a);
 
 int main(void)
 {
@@ -30,7 +32,8 @@ int main(void)
         printf("1. Enter 'I' to enter input\n");
         printf("2. Enter 'O' to see output\n");
         printf("3. Enter 'Q' to quit\n");
-        printf("4. Enter 'C' to change the data");
+        printf("4. Enter 'C' to change the data\n");
+        printf("5. Enter 'S' to search\n");
         printf("\n\nEnter your choice : ");
         char c;
         scanf("%c", &c);
@@ -53,7 +56,47 @@ int main(void)
         }
         else if (c == 'C')
         {
-            printf("\nYou have wanted to change a record\n");
+            printf("\nYou have wanted to change a record\n\n");
+        }
+        else if (c == 'S')
+        {
+            FILE *fr = fopen("student.dat", "rb");
+            while (1)
+            {
+                printf("\n\n1. Enter 'N' to search by name\n");
+                printf("2. Enter 'R' to search by roll\n");
+                printf("3. Enter 'B' to return to main menu\n");
+                printf("\nEnter your choice : ");
+                char c1;
+                while (getchar() != '\n');
+                scanf("%c", &c1);
+                printf("\n");
+                if (c1 == 'N')
+                {
+                    char name[100];
+                    printf("Enter the name : ");
+                    while (getchar() != '\n');
+                    scanf("%[^\n]s", name);
+                    search_by_name(fr, name);
+                }
+                else if (c1 == 'R')
+                {
+                    char roll[13];
+                    printf("Enter the roll : ");
+                    while (getchar() != '\n');
+                    scanf("%s", roll);
+                    search_by_roll(fr, roll);
+                }
+                else if (c1 == 'B')
+                {
+                    break;
+                }
+                else
+                {
+                    printf("Wrong input\n\n");
+                }
+            }
+            fclose(fr);
         }
         else
         {
@@ -78,7 +121,7 @@ void entry_no(FILE *fr)
 void input(FILE *fa)
 {
     int count;
-    printf("\nEnter the number of inputs you will be giving : ");
+    printf("\nEnter the number of records you will enter : ");
     scanf("%d", &count);
     student s;
     for(int  i = 0;i < count;i++)
@@ -107,4 +150,46 @@ void output(FILE *fr)
         fread(&s, sizeof(s), 1, fr);
     }
     printf("\n\n\n");
+}
+
+void search_by_roll(FILE *fr, char *roll)
+{
+    printf("\n");
+    student s;
+    fread(&s, sizeof(s), 1, fr);
+    while(!feof(fr))
+    {
+        if(!strcmp(s.roll, roll))
+        {
+            printf("SL\t\tSTUDENT_NAME\t\tROLL_NO\n");
+            printf("%-3d\t\t%-20s\t%-20s\n", s.sl, s.name, s.roll);
+            rewind(fr);
+            return;
+        }
+        fread(&s, sizeof(s), 1, fr);
+    }
+    rewind(fr);
+    printf("No record of that roll exists\n");
+    return;
+}
+
+void search_by_name(FILE *fr, char *name)
+{
+    printf("\n");
+    student s;
+    fread(&s, sizeof(s), 1, fr);
+    while(!feof(fr))
+    {
+        if(!strcmp(s.name, name))
+        {
+            printf("SL\t\tSTUDENT_NAME\t\tROLL_NO\n");
+            printf("%-3d\t\t%-20s\t%-20s\n", s.sl, s.name, s.roll);
+            rewind(fr);
+            return;
+        }
+        fread(&s, sizeof(s), 1, fr);
+    }
+    rewind(fr);
+    printf("No record of that name exists\n");
+    return;
 }
